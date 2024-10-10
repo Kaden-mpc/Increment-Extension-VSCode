10aea0042e7a5a80766c74a38ff3d94a31100197
Kaden Morrison <kaden@mpconsultants.org>
2024-10-10 15:34:27 +0000

    Made command logic more generic

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,16 @@
 import { printf } from "fast-printf";
 import * as vscode from "vscode";
 
-async function getInput(prompt: string, options: vscode.InputBoxOptions) {
-  return await vscode.window.showInputBox({
-    prompt: prompt,
-    ignoreFocusOut: true,
-    ...options,
-  });
-}
+const DEFAULT_FORMAT_STRING = "%d";
+const PROMPT_START_NUMBER = "Enter start number";
+const PROMPT_INCREMENT_NUMBER = "Enter increment number";
+const PROMPT_FORMAT_STRING = "Enter C format string (e.g. %d)";
 
-function cstringFormat(format: string, n: number) {
+export function cstringFormat(format: string, n: number) {
   return printf(format, n);
 }
 
-function validateFormatString(format: string) {
-  return cstringFormat(format, 1) !== format;
-}
-
-function getAccuracy(n: number) {
+export function getAccuracy(n: number) {
   const str = n.toString();
   const index = str.indexOf(".");
   if (index === -1) {
@@ -26,80 +19,138 @@ function getAccuracy(n: number) {
   return str.length - index - 1;
 }
 
-function multiply(a: number, b: number) {
+export function multiply(a: number, b: number) {
   const accuracy = Math.max(getAccuracy(a), getAccuracy(b));
   return parseFloat((a * b).toFixed(accuracy));
 }
 
-function divide(a: number, b: number) {
+export function add(a: number, b: number) {
   const accuracy = Math.max(getAccuracy(a), getAccuracy(b));
-  return parseFloat((a / b).toFixed(accuracy));
+  return parseFloat((a + b).toFixed(accuracy));
 }
 
-const defaultFormatString = "%d";
+type Preset = {
+  /**
+   * Start number. If not provided, it will prompt the user for input
+   */
+  start?: number;
+  /**
+   * Placeholder for start number. Only used if start is not provided
+   */
+  startPlaceholder?: string;
+  /**
+   * Increment number. If not provided, it will prompt the user for input
+   */
+  increment?: number;
+  /**
+   * Placeholder for increment number. Only used if increment is not provided
+   */
+  incrementPlaceholder?: string;
+  /**
+   * Format string. If not provided, it will prompt the user for input
+   */
+  format?: string;
+  /**
+   * Placeholder for format string. Only used if format is not provided
+   */
+  formatPlaceholder?: string;
+};
 
-export function activate(context: vscode.ExtensionContext) {
-  const disposableIncrement = vscode.commands.registerCommand(
-    "increment-extension.increment",
-    async () => {
-      const startNumber = await getInput("Enter start number", {
-        value: "0",
+const presets: { [key: string]: Preset } = {
+  increment: {
+    startPlaceholder: "0",
+    incrementPlaceholder: "1",
+    formatPlaceholder: DEFAULT_FORMAT_STRING,
+  },
+  incrementIndexed0: {
+    start: 0,
+    increment: 1,
+    format: "%d",
+  },
+  incrementIndexed1: {
+    start: 1,
+    increment: 1,
+    format: "%d",
+  },
+};
+
+async function getInput(prompt: string, options: vscode.InputBoxOptions) {
+  return await vscode.window.showInputBox({
+    prompt: prompt,
+    ignoreFocusOut: true,
+    ...options,
+  });
+}
+
+async function promptForNumber(message: string, placeholder: string) {
+  const input = await getInput(message, {
+    value: placeholder,
+    validateInput(value) {
+      return isNaN(parseFloat(value)) ? "Invalid number" : null;
+    },
+  });
+  if (input === undefined || isNaN(parseFloat(input))) {
+    vscode.window.showErrorMessage("Invalid number");
+    throw new Error("Invalid number");
+  }
+  return parseFloat(input);
+}
+
+function registerCommand(cmd: string, preset: Preset) {
+  return vscode.commands.registerCommand(cmd, async () => {
+    let start = preset.start ?? 0;
+    if (preset.start === undefined && preset.startPlaceholder) {
+      start = await promptForNumber(
+        PROMPT_START_NUMBER,
+        preset.startPlaceholder
+      );
+    }
+
+    let increment = preset.increment ?? 1;
+    if (preset.increment === undefined && preset.incrementPlaceholder) {
+      increment = await promptForNumber(
+        PROMPT_INCREMENT_NUMBER,
+        preset.incrementPlaceholder
+      );
+    }
+
+    let format = preset.format ?? DEFAULT_FORMAT_STRING;
+    if (preset.format === undefined && preset.formatPlaceholder) {
+      const returnedFormat = await getInput(PROMPT_FORMAT_STRING, {
+        value: preset.formatPlaceholder,
       });
-      if (!startNumber || isNaN(parseFloat(startNumber))) {
-        vscode.window.showErrorMessage("Invalid number");
-        return;
-      }
-      const startNumberInt = parseFloat(startNumber);
 
-      // Ask the user for increment number
-      const incrementNumber = await getInput("Enter increment number", {
-        value: "1",
-      });
-
-      if (!incrementNumber || isNaN(parseFloat(incrementNumber))) {
-        vscode.window.showErrorMessage("Invalid number");
-        return;
-      }
-      const incrementNumberInt = parseFloat(incrementNumber);
-
-      // Ask the user for a c format string
-      const formatString = await getInput("Enter format string", {
-        value: defaultFormatString,
-      });
-
-      if (!formatString) {
+      if (!returnedFormat) {
         vscode.window.showErrorMessage("Invalid format string");
         return;
       }
 
-      if (!validateFormatString(formatString)) {
-        vscode.window.showWarningMessage("Format result is same as input");
-      }
-
-      applyIncrement(startNumberInt, incrementNumberInt, formatString);
+      format = returnedFormat;
     }
-  );
 
-  const disposableAutoIncrement0 = vscode.commands.registerCommand(
-    "increment-extension.increment-indexed-0",
-    async () => {
-      applyIncrement(0, 1, "%d");
-    }
-  );
-
-  const disposableAutoIncrement1 = vscode.commands.registerCommand(
-    "increment-extension.increment-indexed-1",
-    async () => {
-      applyIncrement(1, 1, "%d");
-    }
-  );
-
-  context.subscriptions.push(disposableIncrement);
-  context.subscriptions.push(disposableAutoIncrement0);
-  context.subscriptions.push(disposableAutoIncrement1);
+    await applyIncrement(start, increment, format);
+  });
 }
 
-export function applyIncrement(
+function registerCommands(context: vscode.ExtensionContext) {
+  for (const [cmd, preset] of Object.entries(presets)) {
+    context.subscriptions.push(registerCommand(cmd, preset));
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  registerCommands(context);
+}
+
+export function getIncrementedNumber(
+  start: number,
+  increment: number,
+  index: number
+) {
+  return add(multiply(increment, index), start);
+}
+
+async function applyIncrement(
   start: number,
   increment: number,
   format: string
@@ -116,8 +167,8 @@ export function applyIncrement(
   for (let i = 0; i < cursors.length; i++) {
     const cursor = cursors[i];
     const insertPosition = cursor.active.character;
-    const offset = increment + start;
-    const newNumber = cstringFormat(format, multiply(i, offset));
+    const incremented = getIncrementedNumber(start, increment, i);
+    const newNumber = cstringFormat(format, incremented);
     edit.insert(
       editor.document.uri,
       new vscode.Position(cursor.active.line, insertPosition),
@@ -125,7 +176,7 @@ export function applyIncrement(
     );
   }
 
-  vscode.workspace.applyEdit(edit);
+  await vscode.workspace.applyEdit(edit);
 }
 
 export function deactivate() {}
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,14 +1,68 @@
-import * as assert from 'assert';
-
+import * as assert from "assert";
 import * as vscode from "vscode";
 
-// TODO: Add tests
+import {
+  cstringFormat,
+  getAccuracy,
+  getIncrementedNumber,
+  multiply,
+  add,
+} from "../extension";
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite("Increment Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Running Increment extension tests");
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test("getAccuracy", () => {
+    assert.strictEqual(0, getAccuracy(1));
+    assert.strictEqual(0, getAccuracy(1.0));
+    assert.strictEqual(1, getAccuracy(1.1));
+    assert.strictEqual(1, getAccuracy(1.9));
+    assert.strictEqual(2, getAccuracy(1.01));
+    assert.strictEqual(2, getAccuracy(1.99));
+  });
+
+  test("multiply", () => {
+    assert.strictEqual(0, multiply(1, 0));
+    assert.strictEqual(2, multiply(1, 2));
+    assert.strictEqual(2, multiply(1.0, 2));
+    assert.strictEqual(2, multiply(1, 2.0));
+    assert.strictEqual(2, multiply(1.0, 2.0));
+    assert.strictEqual(2.2, multiply(1.1, 2));
+    assert.strictEqual(2.2, multiply(1, 2.2));
+    assert.strictEqual(2.2, multiply(1.1, 2.0));
+    assert.strictEqual(2.2, multiply(1.0, 2.2));
+    assert.strictEqual(2.4, multiply(1.1, 2.2));
+  });
+
+  test("add", () => {
+    assert.strictEqual(1, add(1, 0));
+    assert.strictEqual(3, add(1, 2));
+    assert.strictEqual(3, add(1.0, 2));
+    assert.strictEqual(3, add(1, 2.0));
+    assert.strictEqual(3, add(1.0, 2.0));
+    assert.strictEqual(3.1, add(1.1, 2));
+    assert.strictEqual(3.1, add(1, 2.1));
+    assert.strictEqual(3.1, add(1.1, 2.0));
+    assert.strictEqual(3.1, add(1.0, 2.1));
+    assert.strictEqual(3.3, add(1.1, 2.2));
+  });
+
+  test("cstringFormat", () => {
+    assert.strictEqual("1", cstringFormat("%d", 1));
+    assert.strictEqual("1", cstringFormat("%d", 1.0));
+    assert.strictEqual("1.1", cstringFormat("%f", 1.1));
+    assert.strictEqual("1.1", cstringFormat("%f", 1.1));
+    assert.strictEqual("1.1", cstringFormat("%.1f", 1.1));
+    assert.strictEqual("0x01", cstringFormat("0x%02x", 1));
+    assert.strictEqual("0x0a", cstringFormat("0x%02x", 10));
+  });
+
+  test("getIncrementedNumber", () => {
+    assert.strictEqual(0, getIncrementedNumber(0, 1, 0));
+    assert.strictEqual(1, getIncrementedNumber(0, 1, 1));
+    assert.strictEqual(2, getIncrementedNumber(0, 1, 2));
+    assert.strictEqual(1, getIncrementedNumber(1, 1, 0));
+    assert.strictEqual(2, getIncrementedNumber(1, 1, 1));
+    assert.strictEqual(0.1, getIncrementedNumber(0, 0.1, 1));
+  });
 });
